7e668ba92f7fb1bb614e82f19a68d0c28143246e
Filip Wetterdal Todorovic <81308705+Quroz@users.noreply.github.com>
2023-02-02 22:13:19 +0100

    Creating add/get request

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,1 +1,35 @@
 const Todo = require("../models/todoModel")
+const mongoose = require("mongoose")
+const { findOne } = require("../models/todoModel")
+
+async function get(req,res) {
+
+    try {
+        const todo = await Todo.find()
+        return res.status(200).json(todo)
+    } catch (error) {
+        return res.status(500).json({errorMessage: error.message})
+    }
+}
+
+
+async function add(req,res) {
+    const {title, description} = req.body
+
+    try {
+        const existTitle = await findOne({title})
+
+        if(existTitle)  //Check if title already exists
+           return res.status(400).json({message: "This title already exists"})
+        
+        const addedTodo = await Todo.create({   //Creating new todo
+            title,
+            description
+        })
+
+       return res.status(200).json(addedTodo)
+       
+    } catch (error) {
+        return res.status(500).json({errorMessage: error.message})
+    }
+}
