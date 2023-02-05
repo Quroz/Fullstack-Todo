9117a6f981554221c14093e07305273e583d681c
Filip Wetterdal Todorovic <81308705+Quroz@users.noreply.github.com>
2023-02-05 21:22:46 +0100

    Fixed issue

--- frontend/src/components/AddTodo.jsx
+++ frontend/src/components/AddTodo.jsx
@@ -7,16 +7,18 @@ function AddTodo({ setIsAddButton, isAddButton }) {
     const [errorMessage, setErrorMessage] = useState("")
 
     function onClickHandler() {
-        setTitle("")
+
         axios.post("http://localhost:4000/api/todo/", {
             title: title,
             description: "placeholder"
-        }).then((res) => console.log(res)).catch((err) => console.log(err.response.data));
-        Window.location.reload()
+        }).then((res) => console.log(res))
+            .catch((err) =>
+                console.log(err.response.data));
+        setTitle("")
+        //window.location.reload();
     }
 
     return (
-
         <div className='bg-gray-400 absolute rounded-lg p-7 z-50 left-[20%] top-[30%] w-[300px]'>
             <div className='flex flex-col gap-2 relative'>
                 <div className='h-[8px] w-[8px] bg-black absolute right-0 rounded-full cursor-pointer'
@@ -27,7 +29,7 @@ function AddTodo({ setIsAddButton, isAddButton }) {
                     <input className='rounded-md indent-2 outline-none py-1 shadow-2xl' type="text"
                         onChange={(e) => setTitle(e.target.value)}
                     />
-                    <h1 className='text-red-500 font-medium text-xs'>{errorMessage}</h1>
+                    <h1 className='text-red-500 font-thin text-xs'>{errorMessage}</h1>
                 </div>
                 <button className='bg-blue-500 rounded-3xl py-2 w-[50%] text-white text-sm px-3 hover:bg-blue-400'
                     onClick={onClickHandler}
